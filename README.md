64585dd70a29db035e75d3df2415496a8fa23583
Gerrit Plehn <gerritplehn@gmail.com>
2020-01-26 02:14:27 +0100

    switched to 2-space indentation

--- README.md
+++ README.md
@@ -2,25 +2,25 @@
 ```js
 const TEE = require('./twitch-event-emitter');
 const config = {
-    channels: [{
-        name: 'someChannelName',
-        user: 'someUserName',
-        commands: [{
-                heist: [{
-                        state: 'started',
-                        regex: /Heist started with (?<someNumber>[0-9]+ participants)/
-                    },
-                    {
-                        state: 'finished',
-                        regex: /The heist is finished/
-                    }
-                ]
-            }
+  channels: [{
+    name: 'someChannelName',
+    user: 'someUserName',
+    commands: [{
+        heist: [{
+            state: 'started',
+            regex: /Heist started with (?<someNumber>[0-9]+ participants)/
+          },
+          {
+            state: 'finished',
+            regex: /The heist is finished/
+          }
         ]
-    }]
+      }
+    ]
+  }]
 }
 let events = new TEE(config);
 events.someChannelName.heist.on('started', {someNumber} => {
-    console.log(`a heist with ${someNumber} people was started`);
+  console.log(`a heist with ${someNumber} people was started`);
 });
 ```
--- index.js
+++ index.js
@@ -1,47 +1,47 @@
 const tmi = require('tmi.js');
 const EventEmitter = require('events');
 class TwitchEventEmitter {
-    constructor(twitchEventConfig) {
-        this.tmiConfig = {
-            connection: {
-                secure: true,
-                reconnect: true
-            },
-            channels: []
-        };
-        for (const channel of twitchEventConfig.channels) {
-            this.tmiConfig.channels.push(channel.name);
-            TwitchEventEmitter.prototype[channel.name] = {
-                channelName: channel.name,
-                userName: channel.user
-            };
-            TwitchEventEmitter.prototype[channel.name].commands = [];
-            for (const command of channel.commands) {
-                TwitchEventEmitter.prototype[channel.name][Object.keys(command)[0]] = new class T extends EventEmitter {
-                    constructor(states) {
-                        super();
-                        this.states = states;
-                    };
-                    checkmessage = (message) => {
-                        this.states.forEach((state, matches) => {
-                            if ((matches = message.match(state.regex)) !== null) {
-                                this.emit(state.state, matches.groups ? matches.groups : undefined);
-                            }
-                        });
-                    }
-                }(command[Object.keys(command)[0]]);
-                TwitchEventEmitter.prototype[channel.name].commands.push(TwitchEventEmitter.prototype[channel.name][Object.keys(command)[0]]);
-            }
-        }
-        this.tmiClient = new tmi.Client(this.tmiConfig);
-        this.tmiClient.connect();
-        this.tmiClient.on('message', (channel, userstate, message, self) => {
-            channel = channel.replace('#', '');
-            const channelObject = TwitchEventEmitter.prototype[channel];
-            if (channelObject.userName === userstate.username) {
-                channelObject.commands.forEach((emitter) => emitter.checkmessage(message));
-            }
-        })
+  constructor(twitchEventConfig) {
+    this.tmiConfig = {
+      connection: {
+        secure: true,
+        reconnect: true
+      },
+      channels: []
+    };
+    for (const channel of twitchEventConfig.channels) {
+      this.tmiConfig.channels.push(channel.name);
+      TwitchEventEmitter.prototype[channel.name] = {
+        channelName: channel.name,
+        userName: channel.user
+      };
+      TwitchEventEmitter.prototype[channel.name].commands = [];
+      for (const command of channel.commands) {
+        TwitchEventEmitter.prototype[channel.name][Object.keys(command)[0]] = new class T extends EventEmitter {
+          constructor(states) {
+            super();
+            this.states = states;
+          };
+          checkmessage = (message) => {
+            this.states.forEach((state, matches) => {
+              if ((matches = message.match(state.regex)) !== null) {
+                this.emit(state.state, matches.groups ? matches.groups : undefined);
+              }
+            });
+          }
+        }(command[Object.keys(command)[0]]);
+        TwitchEventEmitter.prototype[channel.name].commands.push(TwitchEventEmitter.prototype[channel.name][Object.keys(command)[0]]);
+      }
     }
+    this.tmiClient = new tmi.Client(this.tmiConfig);
+    this.tmiClient.connect();
+    this.tmiClient.on('message', (channel, userstate, message, self) => {
+      channel = channel.replace('#', '');
+      const channelObject = TwitchEventEmitter.prototype[channel];
+      if (channelObject.userName === userstate.username) {
+        channelObject.commands.forEach((emitter) => emitter.checkmessage(message));
+      }
+    })
+  }
 }
 module.exports = TwitchEventEmitter;
